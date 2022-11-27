a2fcd45d30cfd0039e3bb3bfc9a179636a8b3515
matrn <trnka.matej@seznam.cz>
2022-11-27 12:39:46 +0100

    debugging dsp

--- AcousticDetection_SW/src/main.cpp
+++ AcousticDetection_SW/src/main.cpp
@@ -14,19 +14,20 @@
 #include "../lib/dsp/dsp.hpp"
 
 
-// ----------SETTINGS----------
-const double sound_speed = 343;		 // [m/s]
-const double mics_distance = 0.145;	 // [m]  = distance between microphones in meters
-// ----------SETTINGS----------
 
-const double Ts = 1. / I2S_SAMPLE_RATE;	 // sample time
 
-const double travel_time_for_max_angle = mics_distance / sound_speed;	// how long does it take for sound wave to travel from one microphone to the other
+#define AUDIO_CAPTURE_SERVER_ENABLED
 
-const double correlation_window_time = travel_time_for_max_angle * 50.0;					// 140% of maximum time difference between audio signals
-const int correlation_window_samples_num = correlation_window_time / Ts + 0.5;	// how many samples should we take for cross correlation computation
+#ifdef AUDIO_CAPTURE_SERVER_ENABLED
+	#define AUDIO_CAPTURE_SERVER_URL "http://192.168.0.100:8888/i2s_data_stereo"
+	#define SAMPLES_PER_REQ 20480
+	#include <HTTPClient.h>
+	unsigned long last_audio_capture_conn_check_time = 0;
+	WiFiClient wifiClient;
+	HTTPClient httpClient;
+	WiFiClient audio_capture_server_client;
+#endif
 
-const int max_shift_samples_num = travel_time_for_max_angle/Ts+0.99;   // number of samples needed for maximum time, 0.99 = round up
 // #define ADC_INPUT1 ADC1_CHANNEL_4	 // pin 32
 // #define ADC_INPUT2 ADC1_CHANNEL_5	 // pin 33
 
@@ -61,47 +62,156 @@ void dsp_func(void *param) {
 		// continue;
 		// 
 		// delay(10000);
-		int count = sampler->read(); //[](audio_sample_t left, audio_sample_t right){
-		for(int i = 0; i < count; i ++){
-			audio_sample_t left = sampler->left_channel_data[i];
-			audio_sample_t right = sampler->right_channel_data[i];
+		
 
-			x1.push(left);
-			x2.push(right);
+		#ifdef AUDIO_CAPTURE_SERVER_ENABLED
+			// int sampled = 0;
+			// while(sampled < SAMPLES_PER_REQ){
+				int count = sampler->read(true); //[](audio_sample_t left, audio_sample_t right){
+				//sampled += count;
+				
+			// Serial.println("-------LEFT-------");
+			// for(int i = 0; i < count; i ++) Serial.printf("%d,", sampler->left_channel_data[i]);
+			// Serial.println("------------------");
 
-			//if(count%100 != 0) continue;
-			int N = -1;
-
-			auto corr1 = dsp.correlation<correlation_window_samples_num>(x1, x2, correlation_window_samples_num, max_shift_samples_num+1);
-			auto corr2 = dsp.correlation<correlation_window_samples_num>(x2, x1, correlation_window_samples_num, max_shift_samples_num+1);
-			
-			if(corr1.first != -1 && corr2.first != -1){
-				if(corr1.second > corr2.second) N = corr1.first;
-				else N = -corr2.first;
-			}
-			else if(corr1.first != -1) N = corr1.first;
-			else if(corr2.first != -1) N = corr2.first;
-			if(N != -1 && N != 0){
+			// Serial.println("-------RIGHT-------");
+			// for(int i = 0; i < count; i ++) Serial.printf("%d,", sampler->right_channel_data[i]);
+			// Serial.println("-------------------");
+			//if(audio_capture_server_client.connected()){
 				digitalWrite(LED_BUILTIN, HIGH);
-				
-				double tau = N*(1./I2S_SAMPLE_RATE);
-				int angle = dsp.rad2deg( acos((tau*sound_speed)/mics_distance) )+0.5;
-				char str[10];
-				
-				sprintf(str, "%d", angle);
-				Serial.printf("%d, %f s, %d\n", N, tau, angle);
-				ws.textAll(str);
+				httpClient.begin(wifiClient, AUDIO_CAPTURE_SERVER_URL);
+				httpClient.addHeader("content-type", "application/octet-stream");
+				uint8_t data[count*sizeof(int16_t)*2];
+				memcpy(data, sampler->left_channel_data, count*sizeof(int16_t));
+				memcpy(data+count*sizeof(int16_t), sampler->right_channel_data, count*sizeof(int16_t));
+
+				// for(int i = 0; i < count*2; i ++){
+				// 	Serial.printf("%d, ", ((int16_t*)data)[i]);
+				// }
+				// Serial.println();
+				httpClient.POST(data, count*2*sizeof(int16_t));
+				httpClient.end();
 				digitalWrite(LED_BUILTIN, LOW);
+				// for(int i = 0; i < count; i ++){
+				// 	audio_sample_t left = sampler->left_channel_data[i];
+				// 	audio_sample_t right = sampler->right_channel_data[i];
+
+				// 	audio_capture_server_client.write(sampler->left_channel_data, count*sizeof(audio_sample_t));
+				// 	audio_capture_server_client.write(sampler->right_channel_data, count*sizeof(audio_sample_t));
+			//}
+		
+		#else
+			int count = sampler->read(); //[](audio_sample_t left, audio_sample_t right){
+
+			for(int i = 0; i < count; i ++){
+				audio_sample_t left = sampler->left_channel_data[i];
+				audio_sample_t right = sampler->right_channel_data[i];
+
+				x1.push(left);
+				x2.push(right);
+
+				//if(count%100 != 0) continue;
+				int N = -1;
+
+				auto corr1 = dsp.correlation<correlation_window_samples_num>(x1, x2, correlation_window_samples_num, max_shift_samples_num+1);
+				auto corr2 = dsp.correlation<correlation_window_samples_num>(x2, x1, correlation_window_samples_num, max_shift_samples_num+1);
+				
+				if(corr1.first != -1 && corr2.first != -1){
+					if(corr1.second > corr2.second) N = corr1.first;
+					else N = -corr2.first;
+				}
+				else if(corr1.first != -1) N = corr1.first;
+				else if(corr2.first != -1) N = corr2.first;
+				if(N != -1 && N != 0){
+					digitalWrite(LED_BUILTIN, HIGH);
+					
+					double tau = N*(1./I2S_SAMPLE_RATE);
+					int angle = dsp.rad2deg( acos((tau*sound_speed)/mics_distance) )+0.5;
+					char str[10];
+					
+					sprintf(str, "%d", angle);
+					Serial.printf("%d, %f s, %d\n", N, tau, angle);
+					ws.textAll(str);
+					digitalWrite(LED_BUILTIN, LOW);
+				}
 			}
-		}
-		//ws.textAll("AA");
+			//ws.textAll("AA");
+		#endif
 	}
 }
 
 
 
 void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-	
+	if (type == WS_EVT_CONNECT) {
+		Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+		client->printf("Hello Client %u :)", client->id());
+		client->ping();
+	} else if (type == WS_EVT_DISCONNECT) {
+		Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+	} else if (type == WS_EVT_ERROR) {
+		Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+	} else if (type == WS_EVT_PONG) {
+		Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+	} else if (type == WS_EVT_DATA) {
+		AwsFrameInfo *info = (AwsFrameInfo *)arg;
+		String msg = "";
+		if (info->final && info->index == 0 && info->len == len) {
+			// the whole message is in a single frame and we got all of it's data
+			Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+
+			if (info->opcode == WS_TEXT) {
+				for (size_t i = 0; i < info->len; i++) {
+					msg += (char)data[i];
+				}
+			} else {
+				char buff[3];
+				for (size_t i = 0; i < info->len; i++) {
+					sprintf(buff, "%02x ", (uint8_t)data[i]);
+					msg += buff;
+				}
+			}
+			Serial.printf("%s\n", msg.c_str());
+
+			if (info->opcode == WS_TEXT)
+				client->text("I got your text message");
+			else
+				client->binary("I got your binary message");
+		} else {
+			// message is comprised of multiple frames or the frame is split into multiple packets
+			if (info->index == 0) {
+				if (info->num == 0)
+					Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+				Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+			}
+
+			Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+
+			if (info->opcode == WS_TEXT) {
+				for (size_t i = 0; i < len; i++) {
+					msg += (char)data[i];
+				}
+			} else {
+				char buff[3];
+				for (size_t i = 0; i < len; i++) {
+					sprintf(buff, "%02x ", (uint8_t)data[i]);
+					msg += buff;
+				}
+			}
+			Serial.printf("%s\n", msg.c_str());
+
+			if ((info->index + len) == info->len) {
+				Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+				if (info->final) {
+					Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+					if (info->message_opcode == WS_TEXT)
+						client->text("I got your text message");
+					else
+						client->binary("I got your binary message");
+				}
+			}
+		}
+	}
 }
 
 void setup() {
@@ -220,6 +330,30 @@ void setup() {
 	});
 	server.begin();
 
+
+	// #ifdef AUDIO_CAPTURE_SERVER_ENABLED
+    // 	if (!audio_capture_server_client.connect(AUDIO_CAPTURE_SERVER_IP, AUDIO_CAPTURE_SERVER_PORT)) {
+    //     	Serial.println("Connection failed.");
+    // 	}
+  	// #endif 
+	// int count = 2;
+	// mics.left_channel_data[0] = 5895;
+	// mics.left_channel_data[1] = -5555;
+	// mics.right_channel_data[0] = -4206;
+	// mics.right_channel_data[1] = 69;
+	// httpClient.begin(wifiClient, AUDIO_CAPTURE_SERVER_URL);
+	// httpClient.addHeader("content-type", "application/octet-stream");
+	// uint8_t data[count*sizeof(int16_t)*2];
+	// memcpy(data, mics.left_channel_data, count*sizeof(int16_t));
+	// memcpy(data+count*sizeof(int16_t), mics.right_channel_data, count*sizeof(int16_t));
+	// for(int i = 0; i < count*2; i ++){
+	// 	Serial.printf("%d, ", ((int16_t*)data)[i]);
+	// }
+	// Serial.println();
+	// httpClient.POST(data, count*2*sizeof(int16_t));
+	// httpClient.end();
+	// while(1);
+
 	Serial.println("Starting...");
 	xTaskCreatePinnedToCore(
 		dsp_func,  /* Function to implement the task */
@@ -234,7 +368,19 @@ void setup() {
 
 
 
+
 void loop() {
 	ws.cleanupClients();
 	//vTaskDelay()
+
+	// #ifdef AUDIO_CAPTURE_SERVER_ENABLED
+	// 	if(millis()-last_audio_capture_conn_check_time > 3000){
+	// 		last_audio_capture_conn_check_time = millis();
+	// 		if(!audio_capture_server_client.connected()){
+	// 			if (!audio_capture_server_client.connect(AUDIO_CAPTURE_SERVER_IP, AUDIO_CAPTURE_SERVER_PORT)) {
+	// 				Serial.println("Connection failed.");
+	// 			}
+	// 		}
+	// 	}
+	// #endif
 }
