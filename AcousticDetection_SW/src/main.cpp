f7499164307fb7d1e571af8b17f37d0205fb96d9
matrn <trnka.matej@seznam.cz>
2023-03-17 18:33:09 +0100

    refactoring

--- AcousticDetection_SW/src/main.cpp
+++ AcousticDetection_SW/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
@@ -10,31 +9,31 @@
 
 #include "../include/circular_buffer.hpp"
 #include "../include/config.hpp"
-#include "../lib/i2s_mic/mic.hpp"
 #include "../lib/dsp/dsp.hpp"
+#include "../lib/i2s_mic/mic.hpp"
 
 
+#define ENABLE_WS    // WebSockets
+#define ENABLE_SSE   // Server Sent Events
 
 
-//#define AUDIO_CAPTURE_SERVER_ENABLED
+// #define AUDIO_CAPTURE_SERVER_ENABLED
 
 #ifdef AUDIO_CAPTURE_SERVER_ENABLED
-	#define AUDIO_CAPTURE_SERVER_URL "http://192.168.0.100:5005/i2s_samples"   //"http://192.168.26.92:5005/i2s_samples"
-	//#define AUDIO_CAPTURE_SERVER_URL "http://10.42.0.1:5005/i2s_samples"
-	#define SAMPLES_PER_REQ 1024*50
+	#define AUDIO_CAPTURE_SERVER_URL "http://192.168.0.100:5005/i2s_samples"  //"http://192.168.26.92:5005/i2s_samples"
+	// #define AUDIO_CAPTURE_SERVER_URL "http://10.42.0.1:5005/i2s_samples"
+	#define SAMPLES_PER_REQ 1024 * 50
 	#include <HTTPClient.h>
 	unsigned long last_audio_capture_conn_check_time = 0;
 	WiFiClient wifiClient;
 	HTTPClient httpClient;
-	//WiFiClient audio_capture_server_client;
-	//CircularBuffer<uint8_t *, 2> data_for_send;
-	uint8_t* data_for_send;
+	// WiFiClient audio_capture_server_client;
+	// CircularBuffer<uint8_t *, 2> data_for_send;
+	uint8_t *data_for_send;
 #endif
 
 // #define ADC_INPUT1 ADC1_CHANNEL_4	 // pin 32
 // #define ADC_INPUT2 ADC1_CHANNEL_5	 // pin 33
-
-
 
 // #define SAMPLES_NUM 512
 
@@ -53,33 +52,50 @@ DSP dsp;
 
 // SKETCH BEGIN
 AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-AsyncEventSource events("/events");
+#ifdef ENABLE_WS
+	AsyncWebSocket ws("/ws");
+#endif
+
+#ifdef ENABLE_SSE
+	AsyncEventSource events("/events");
+#endif
 
 OnsetDetector od1;
 OnsetDetector od2;
 
 
+void send_angle(int angle){
+	char str[10];
+	sprintf(str, "%d", angle);
+	#ifdef ENABLE_WS
+		ws.textAll(str);
+	#endif
+
+	#ifdef ENABLE_SSE
+		events.send(str, "angle");
+	#endif
+}
+
+
 void dsp_func(void *param) {
 	Serial.printf("DSP running on core: %d\n", xPortGetCoreID());
-	LRMics *sampler = (LRMics *)(param); 
+	LRMics *sampler = (LRMics *)(param);
 
 	int qq = 0;
 	bool capture_started = false;
 	while (true) {
 		// mics.read_and_print();
 		// continue;
-		// 
+		//
 		// delay(10000);
-		
 
-		#ifdef AUDIO_CAPTURE_SERVER_ENABLED
-			int sampled = 0;
-			int ppp = 0;
-			while(sampled < SAMPLES_PER_REQ){
-				int count = sampler->read(true); //[](audio_sample_t left, audio_sample_t right){
-				
-				Serial.printf("%d, %d\n", count, sampled);				
+#ifdef AUDIO_CAPTURE_SERVER_ENABLED
+		int sampled = 0;
+		int ppp = 0;
+		while (sampled < SAMPLES_PER_REQ) {
+			int count = sampler->read(true);  //[](audio_sample_t left, audio_sample_t right){
+
+			Serial.printf("%d, %d\n", count, sampled);
 			// Serial.println("-------LEFT-------");
 			// for(int i = 0; i < count; i ++) Serial.printf("%d,", sampler->left_channel_data[i]);
 			// Serial.println("------------------");
@@ -87,163 +103,102 @@ void dsp_func(void *param) {
 			// Serial.println("-------RIGHT-------");
 			// for(int i = 0; i < count; i ++) Serial.printf("%d,", sampler->right_channel_data[i]);
 			// Serial.println("-------------------");
-			//if(audio_capture_server_client.connected()){
-				
-				
-				memcpy(data_for_send+sampled*sizeof(int16_t), sampler->data, count*sizeof(int16_t));
-				sampled += count;
-				// for(int i = 0; i < count*2; i ++){
-				// 	Serial.printf("%d, ", ((int16_t*)data)[i]);
-				// }
-				// Serial.println();
-				if(ppp == 5) httpClient.begin(wifiClient, AUDIO_CAPTURE_SERVER_URL);
-				if(ppp == 6) httpClient.addHeader("content-type", "application/octet-stream");
-				ppp ++;
+			// if(audio_capture_server_client.connected()){
+
+			memcpy(data_for_send + sampled * sizeof(int16_t), sampler->data, count * sizeof(int16_t));
+			sampled += count;
+			// for(int i = 0; i < count*2; i ++){
+			// 	Serial.printf("%d, ", ((int16_t*)data)[i]);
+			// }
+			// Serial.println();
+			if (ppp == 5) httpClient.begin(wifiClient, AUDIO_CAPTURE_SERVER_URL);
+			if (ppp == 6) httpClient.addHeader("content-type", "application/octet-stream");
+			ppp++;
+		}
+		// data_for_send.push(data);
+
+		digitalWrite(LED_BUILTIN, HIGH);
+
+		httpClient.POST(data_for_send, sampled * sizeof(int16_t));
+		httpClient.end();
+		digitalWrite(LED_BUILTIN, LOW);
+		// free(data);
+
+		// for(int i = 0; i < count; i ++){
+		// 	audio_sample_t left = sampler->left_channel_data[i];
+		// 	audio_sample_t right = sampler->right_channel_data[i];
+
+		// 	audio_capture_server_client.write(sampler->left_channel_data, count*sizeof(audio_sample_t));
+		// 	audio_capture_server_client.write(sampler->right_channel_data, count*sizeof(audio_sample_t));
+		//}
+
+#else
+		int count = sampler->read();  //[](audio_sample_t left, audio_sample_t right){
+
+		for (int i = 0; i < count; i++) {
+			audio_sample_t left = sampler->data[i];
+			audio_sample_t right = sampler->data[++i];
+
+			if (od1.detect(left) && od2.detect(right) && capture_started == false) {
+				Serial.println("Capture started");
+				capture_started = true;
 			}
-			//data_for_send.push(data);
 
-				digitalWrite(LED_BUILTIN, HIGH);
-				
-				httpClient.POST(data_for_send, sampled*sizeof(int16_t));
-				httpClient.end();
-				digitalWrite(LED_BUILTIN, LOW);
-				//free(data);
+			if (capture_started) {
+				x1.push(left);
+				x2.push(right);
+			}
+			// if(qq++ < 512) continue;
+			// qq = 0;
 
+			if (capture_started && ++qq >= CORR_SIZE) {
+				Serial.println("START PROCESSING");
+				qq = 0;
+				capture_started = false;
 
-				// for(int i = 0; i < count; i ++){
-				// 	audio_sample_t left = sampler->left_channel_data[i];
-				// 	audio_sample_t right = sampler->right_channel_data[i];
+				std::pair<int, double> xcorr_peak;
+				bool xcorr_peak_found = dsp.xcorr_max<CircularBuffer<audio_sample_t, CORR_SIZE> >(x1, x2, xcorr_peak, CORR_SIZE, max_shift_samples_num + 1, true);
 
-				// 	audio_capture_server_client.write(sampler->left_channel_data, count*sizeof(audio_sample_t));
-				// 	audio_capture_server_client.write(sampler->right_channel_data, count*sizeof(audio_sample_t));
-			//}
-		
-		#else
-			int count = sampler->read(); //[](audio_sample_t left, audio_sample_t right){
+				Serial.println(xcorr_peak_found);
+				if (xcorr_peak_found) {
+					digitalWrite(LED_BUILTIN, HIGH);
 
-			for(int i = 0; i < count; i ++){
-				audio_sample_t left = sampler->data[i];
-				audio_sample_t right = sampler->data[++i];
-
-				
-
-				if(od1.detect(left) && od2.detect(right) && capture_started == false){
-					Serial.println("Capture started");
-					capture_started = true;
-				}
-
-				if(capture_started){
-					x1.push(left);
-					x2.push(right);
-				}
-				// if(qq++ < 512) continue;
-				// qq = 0;
-				
-				if(capture_started && ++qq >= CORR_SIZE){
-					Serial.println("START PROCESSING");
-					qq = 0;
-					capture_started = false;
-
-				
-					std::pair<int, double> xcorr_peak;
-					bool xcorr_peak_found = dsp.xcorr_max<CircularBuffer<audio_sample_t, CORR_SIZE>>(x1, x2, xcorr_peak, CORR_SIZE, max_shift_samples_num+1, true);
-					
-					Serial.println(xcorr_peak_found);
-					if(xcorr_peak_found){
-						digitalWrite(LED_BUILTIN, HIGH);
+					double tau = xcorr_peak.first * (1. / I2S_SAMPLE_RATE);
+					if (tau >= travel_time_for_max_angle)
+						Serial.printf("XCORR - shift too big: N: %d, max: %f, tau: %f s\n", xcorr_peak.first, xcorr_peak.second, tau);
+					else {
+						int angle = dsp.rad2deg(dsp.calculate_angle(tau)) + 0.5;
 						
-						double tau = xcorr_peak.first*(1./I2S_SAMPLE_RATE);
-						if(tau >= travel_time_for_max_angle) Serial.printf("XCORR - shift too big: N: %d, max: %f, tau: %f s\n", xcorr_peak.first, xcorr_peak.second, tau);
-						else {
-							int angle = dsp.rad2deg(dsp.calculate_angle(tau))+0.5;
-							char str[10];
-							
-							sprintf(str, "%d", angle);
-							Serial.printf("N: %d, max: %f, tau: %f s, angle: %d\n", xcorr_peak.first, xcorr_peak.second, tau, angle);
-							ws.textAll(str);
-						}
-						digitalWrite(LED_BUILTIN, LOW);
+						Serial.printf("N: %d, max: %f, tau: %f s, angle: %d\n", xcorr_peak.first, xcorr_peak.second, tau, angle);
+
+						send_angle(angle);
 					}
-				}
-			}
-			//ws.textAll("AA");
-		#endif
-	}
-}
-
-
-
-void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-	if (type == WS_EVT_CONNECT) {
-		Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-		client->printf("Hello Client %u :)", client->id());
-		client->ping();
-	} else if (type == WS_EVT_DISCONNECT) {
-		Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
-	} else if (type == WS_EVT_ERROR) {
-		Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
-	} else if (type == WS_EVT_PONG) {
-		Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
-	} else if (type == WS_EVT_DATA) {
-		AwsFrameInfo *info = (AwsFrameInfo *)arg;
-		String msg = "";
-		if (info->final && info->index == 0 && info->len == len) {
-			// the whole message is in a single frame and we got all of it's data
-			Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
-
-			if (info->opcode == WS_TEXT) {
-				for (size_t i = 0; i < info->len; i++) {
-					msg += (char)data[i];
-				}
-			} else {
-				char buff[3];
-				for (size_t i = 0; i < info->len; i++) {
-					sprintf(buff, "%02x ", (uint8_t)data[i]);
-					msg += buff;
-				}
-			}
-			Serial.printf("%s\n", msg.c_str());
-
-			if (info->opcode == WS_TEXT)
-				client->text("I got your text message");
-			else
-				client->binary("I got your binary message");
-		} else {
-			// message is comprised of multiple frames or the frame is split into multiple packets
-			if (info->index == 0) {
-				if (info->num == 0)
-					Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
-				Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
-			}
-
-			Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
-
-			if (info->opcode == WS_TEXT) {
-				for (size_t i = 0; i < len; i++) {
-					msg += (char)data[i];
-				}
-			} else {
-				char buff[3];
-				for (size_t i = 0; i < len; i++) {
-					sprintf(buff, "%02x ", (uint8_t)data[i]);
-					msg += buff;
-				}
-			}
-			Serial.printf("%s\n", msg.c_str());
-
-			if ((info->index + len) == info->len) {
-				Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
-				if (info->final) {
-					Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
-					if (info->message_opcode == WS_TEXT)
-						client->text("I got your text message");
-					else
-						client->binary("I got your binary message");
+					digitalWrite(LED_BUILTIN, LOW);
 				}
 			}
 		}
+		// ws.textAll("AA");
+#endif
 	}
 }
+
+#ifdef ENABLE_WS
+	void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+		if (type == WS_EVT_CONNECT) {
+			Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+			client->printf("Hello Client %u :)", client->id());
+			client->ping();
+		} else if (type == WS_EVT_DISCONNECT) {
+			Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+		} else if (type == WS_EVT_ERROR) {
+			Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+		} else if (type == WS_EVT_PONG) {
+			Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+		} else if (type == WS_EVT_DATA) {
+		}
+	}
+#endif
+
 
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
@@ -255,9 +210,9 @@ void setup() {
 	Serial.printf("setup running on core: %d\n", xPortGetCoreID());
 	mics.init();
 
-	#ifdef AUDIO_CAPTURE_SERVER_ENABLED
-		data_for_send = (uint8_t*)malloc(SAMPLES_PER_REQ*sizeof(int16_t));
-	#endif
+#ifdef AUDIO_CAPTURE_SERVER_ENABLED
+	data_for_send = (uint8_t *)malloc(SAMPLES_PER_REQ * sizeof(int16_t));
+#endif
 
 	// Serial.println(ESP.getFreeHeap());
 	// for(int i = 0; i < 2; i ++){
@@ -274,16 +229,16 @@ void setup() {
 	// Serial.printf("Correlation time: %f us\n", (end-start)/100.);
 	// delay(10000);
 	// while(1);
-	   /* Core where the task should run */
+	/* Core where the task should run */
 
 	Serial.printf("Connecting to: %s\n", wifi_ssid);
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(wifi_ssid, wifi_password);
-	for(int i = 0; i < 3; i ++){
-		if(WiFi.waitForConnectResult() == WL_CONNECTED) break;
-		Serial.print(".");		
+	for (int i = 0; i < 3; i++) {
+		if (WiFi.waitForConnectResult() == WL_CONNECTED) break;
+		Serial.print(".");
 	}
-	if(WiFi.waitForConnectResult() != WL_CONNECTED) {
+	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
 		Serial.println("Connection Failed! Rebooting...");
 		delay(1000);
 		ESP.restart();
@@ -294,14 +249,9 @@ void setup() {
 	MDNS.addService("http", "tcp", 80);
 
 	SPIFFS.begin();
+	
+	
 
-	ws.onEvent(onWsEvent);
-	server.addHandler(&ws);
-
-	events.onConnect([](AsyncEventSourceClient *client) {
-		client->send("hello!", NULL, millis(), 1000);
-	});
-	server.addHandler(&events);
 
 	server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
 		request->send(200, "text/plain", String(ESP.getFreeHeap()));
@@ -355,6 +305,25 @@ void setup() {
 
 		request->send(404);
 	});
+
+	#ifdef ENABLE_WS
+		ws.onEvent(onWsEvent);
+		server.addHandler(&ws);
+	#endif
+
+	#ifdef ENABLE_SSE
+		DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+		events.onConnect([](AsyncEventSourceClient *client){
+			// if(client->lastId()){
+			// 	Serial.printf("Client reconnected! Last message ID that it gat is: %u\n", client->lastId());
+			// }
+			// //send event with message "hello!", id current millis
+			// // and set reconnect delay to 1 second
+			// client->send("hello!",NULL,millis(),1000);
+		});
+		server.addHandler(&events);
+	#endif
+
 	// server.onFileUpload([](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final) {
 	// 	if (!index)
 	// 		Serial.printf("UploadStart: %s\n", filename.c_str());
@@ -371,12 +340,11 @@ void setup() {
 	// });
 	server.begin();
 
-
 	// #ifdef AUDIO_CAPTURE_SERVER_ENABLED
-    // 	if (!audio_capture_server_client.connect(AUDIO_CAPTURE_SERVER_IP, AUDIO_CAPTURE_SERVER_PORT)) {
-    //     	Serial.println("Connection failed.");
-    // 	}
-  	// #endif 
+	// 	if (!audio_capture_server_client.connect(AUDIO_CAPTURE_SERVER_IP, AUDIO_CAPTURE_SERVER_PORT)) {
+	//     	Serial.println("Connection failed.");
+	// 	}
+	// #endif
 	// int count = 2;
 	// mics.left_channel_data[0] = 5895;
 	// mics.left_channel_data[1] = -5555;
@@ -403,16 +371,13 @@ void setup() {
 		&mics,	   /* Task input parameter */
 		2,		   /* Priority of the task */
 		&dsp_task, /* Task handle. */
-		1);		
+		1);
 	digitalWrite(LED_BUILTIN, LOW);
 }
 
-
-
-
 void loop() {
 	ws.cleanupClients();
-	//vTaskDelay()
+	// vTaskDelay()
 
 	// #ifdef AUDIO_CAPTURE_SERVER_ENABLED
 	// 	if(millis()-last_audio_capture_conn_check_time > 3000){
