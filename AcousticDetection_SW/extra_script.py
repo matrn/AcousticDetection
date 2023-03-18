abca6d4d30bc2603215a85c367d52750db0e9e63
matrn <trnka.matej@seznam.cz>
2023-03-18 23:28:23 +0100

    add docs & flush=True for print

--- AcousticDetection_SW/extra_script.py
+++ AcousticDetection_SW/extra_script.py
@@ -1,4 +1,4 @@
-print("EXTRA")
+print("EXTRA Python script for Platformio")
 import subprocess
 import sys
 
@@ -9,9 +9,13 @@ print("Current Build targets", BUILD_TARGETS)
 
 
 def before_buildfs(source, target, env):
-    print(source, target, env)
-    print("Before build fs")
-    subprocess.run(['bash', '../web/bundle_all.sh'], stderr=sys.stderr, stdout=sys.stdout)
+	print(source, target, env)
+	print("Before build fs")
+	subprocess.run(['bash', '../web/bundle_all.sh'], stderr=sys.stderr, stdout=sys.stdout)
 
-env.Execute("$PYTHONEXE -m pip install jsmin")
+
+env.Execute("$PYTHONEXE -m pip install jsmin")   # docs: https://docs.platformio.org/en/stable/scripting/examples/extra_python_packages.html
+
+# docs: https://docs.platformio.org/en/latest/scripting/actions.html
+# issue with buildfs: https://github.com/platformio/platformio-core/issues/3842
 env.AddPreAction("$BUILD_DIR/spiffs.bin", before_buildfs)
--- web/bundle.py
+++ web/bundle.py
@@ -22,44 +22,51 @@ def minify(path):
 def gzip_compress(content):
 	return gzip.compress(bytes(content, 'utf-8'))
 
-print("--- Cleaning out directory ---")
+
+print("--- Cleaning out directory ---", flush=True)
 for file in os.listdir(OUT_DIR):
-	print(f'Removing {file}')
+	print(f'Removing {file}', flush=True)
 	os.remove(OUT_DIR+file)
 
 
-print("--- Starting bundle process ---")
+print("--- Starting bundle process ---", flush=True)
 for file in os.listdir(SRC_DIR):
 	path = SRC_DIR + file
 	path_out = OUT_DIR + file
 
 	if len(file) > 32:
-		print(f'ERROR: file >{file}< - filename length must be <= 32 characters but len(filename) = {len(file)}')
+		print(f'ERROR: file >{file}< - filename length must be <= 32 characters but len(filename) = {len(file)}', flush=True)
 		exit(1)
 
 	if os.path.isfile(path):
 		# minify JS file
 		if file.endswith('.js'):
-			print(f'Minifying {file}')
+			print(f'Minifying {file}', flush=True)
 			with open(path_out, 'w') as out_file:
 				out_file.write(minify(path))
 		
+		# ignore hidden files - for example .gitignore
+		elif file.startswith('.'):
+			print(f'Skipping {file}', flush=True)
+		
 		# just copy
 		else:
-			print(f'Copying {file}')
+			print(f'Copying {file}', flush=True)
 			shutil.copy(path, path_out)
 
-print("--- Starting compression ---")
+
+print("--- Starting compression ---", flush=True)
 for file in os.listdir(OUT_DIR):
 	path = OUT_DIR + file
 
 	if file.endswith(('.js', '.css')):
-		print(f'Compressing {file}')
+		print(f'Compressing {file}', flush=True)
 		with open(path, 'r') as infile:
 			with open(path + '.gz', 'wb') as outfile:
-				#print(gzip_compress(infile.read()))
+				#print(gzip_compress(infile.read()), flush=True)
 				outfile.write(gzip_compress(infile.read()))
 
 		os.remove(path)
 
-print("DONE!")
+
+print("DONE!", flush=True)
