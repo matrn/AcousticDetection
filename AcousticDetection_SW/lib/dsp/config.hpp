e2647437d4136675fcfda6da2b0458fd12dc6001
matrn <trnka.matej@seznam.cz>
2023-05-23 13:35:07 +0200

    changed mics distance to 0.2m (new board)

--- AcousticDetection_SW/lib/dsp/config.hpp
+++ AcousticDetection_SW/lib/dsp/config.hpp
@@ -2,5 +2,5 @@
 
 // ----------SETTINGS----------
 const double SOUND_SPEED = 343;		 // [m/s]
-const double MICS_DISTANCE = 0.186;	 // [m]  = distance between microphones in meters
+const double MICS_DISTANCE = 0.2;	 // [m]  = distance between microphones in meters
 // ----------SETTINGS----------
